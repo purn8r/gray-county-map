74681013bd1e1bf6fb91d7b4551762ff8304bbf2
purn8r <58445021+purn8r@users.noreply.github.com>
2023-10-02 20:34:20 -0600

    fix subpages and format code

--- edit/index.js
+++ edit/index.js
@@ -21,22 +21,17 @@ L.PM.setOptIn(false);
 //       //}
 //     })
 
-// Load the dataset
-var dataset = L.geoJSON(roads, {
-  onEachFeature: onEachFeature,
-  //color: color: "#ff0000"
-})
 
 function highlightFeature(e) {
   var layer = e.target;
 
   layer.setStyle({
     stroke: true,
-    weight: 8,    
+    weight: 8,
     dashArray: '',
     opacity: 0.5,
     color: '#ff3300'
-    
+
   });
 
   if (!L.Browser.ie && !L.Browser.opera) {
@@ -46,7 +41,7 @@ function highlightFeature(e) {
 
 function resetHighlight(e) {
   dataset.resetStyle(e.target);
-	var layer = e.target;
+  var layer = e.target;
 
   layer.setStyle({
     weight: 4,
@@ -72,69 +67,59 @@ function onEachFeature(feature, layer) {
   });
 }
 
+let datasets = []
+let layerList = {}
+data.forEach(json => {
+  var dataset = L.geoJSON(json, {
+    onEachFeature: onEachFeature,
+    //color: color: "#ff0000"
+  })
+  datasets.push(dataset)
+  layerList = { ...layerList, [json.layerName]: dataset }
+
+})
+
 // Add map, set default view
 var map = L.map('map', {
-    crs: L.CRS.Simple,
-    pmIgnore: false,
-    minZoom: -1,
-    center: [0,0],
-    // renderer: streetLabelsRenderer,
-    layers: [dataset],
-}).setView([-300,500], 0);
+  crs: L.CRS.Simple,
+  pmIgnore: false,
+  minZoom: -1,
+  center: [0, 0],
+  // renderer: streetLabelsRenderer,
+  layers: datasets,
+}).setView([-300, 500], 0);
 map.on("pm:create", (e) => {
   e.layer.options.pmIgnore = false;
   L.PM.reInitLayer(e.layer);
 });
 
-map.pm.addControls({  
-  position: 'topleft',  
+map.pm.addControls({
+  position: 'topleft',
   drawCircleMarker: false,
   rotateMode: false,
-});  
+});
 
 // Add tile layer
 var t = L.tileLayer('../tiles/{z}/{x}/{y}.png', {
-    noWrap: true,
-    tileSize: L.point(1021, 865),
-    minNativeZoom: 0,
-    maxNativeZoom: 3,
-    minZoom: -1,
-    maxZoom: 5,
-    bounds: [[0,0],[-864,1020]]
+  noWrap: true,
+  tileSize: L.point(1021, 865),
+  minNativeZoom: 0,
+  maxNativeZoom: 3,
+  minZoom: -1,
+  maxZoom: 5,
+  bounds: [[0, 0], [-864, 1020]]
 }).addTo(map);
 
-L.control.layers({ "Satellite": t /*only 1 can be picked from here, separate with ;*/}, { "Roads": dataset /*can toggle any number, separate with ;*/}).addTo(map)
+L.control.layers({ "Satellite": t /*only 1 can be picked from here, separate with ;*/ }, layerList).addTo(map)
 
-// for testing
-/*
-var popup = L.popup();
-let listString = ""
-let debounce = false
-
-async function onMapClick(e) {
-    if (debounce === false) {
-        debounce = true
-        let initialCoords = e.latlng.toString().split("(")[1].split(")")[0]
-        const splitted = initialCoords.split(", ")
-        initialCoords = "[" + splitted[1] + ", " + splitted[0] + "], "
-        listString = listString + initialCoords
-        console.log(listString)       
-        await new Promise(resolve => setTimeout(resolve, 100))
-        debounce = false
-    }
-    
-}
-map.on('click', onMapClick);
-*/
-//
 
 function exporter() {
-  function generateGeoJson(){
-    var fg = L.featureGroup();    
+  function generateGeoJson() {
+    var fg = L.featureGroup();
     var layers = findLayers(map);
-        layers.forEach(function(layer){
-        fg.addLayer(layer);
-         });
+    layers.forEach(function (layer) {
+      fg.addLayer(layer);
+    });
     console.log(JSON.stringify(fg.toGeoJSON()));
     document.getElementById("export-text").textContent = JSON.stringify(fg.toGeoJSON())
 
@@ -166,4 +151,4 @@ function exporter() {
   generateGeoJson()
 }
 
-function clearExporter() {document.getElementById("export-text").textContent =""}
+function clearExporter() { document.getElementById("export-text").textContent = "" }
